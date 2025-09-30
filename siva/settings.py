3d46b468fb774f623f8f9469f40e163d1f3b6338
vida1380 <vika801380@gmail.com>
2025-09-30 21:49:32 +0330

    Update settings.py

--- siva/settings.py
+++ siva/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATICFILES_URLS = ['static/']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 # Default primary key field type

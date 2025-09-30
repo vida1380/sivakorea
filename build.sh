802724274d63d14a54e61af6e94b15dd8f441eff
vida <vika801380@gmail.com>
2025-09-30 10:41:49 +0330

    fix build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
-python manage.py creatsuperuser --noinput
+python manage.py createsuperuser --noinput
 python manage.py collectstatic --noinput

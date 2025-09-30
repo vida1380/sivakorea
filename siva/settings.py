69b6c385ccba14f4fca8eae1989c5552bba072d3
vida1380 <vika801380@gmail.com>
2025-09-30 22:00:46 +0330

    Update settings.py

--- siva/settings.py
+++ siva/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

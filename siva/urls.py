c0aaa8c4ae4effb3ff6accf7ff414d982cf0908d
vida1380 <vika801380@gmail.com>
2025-10-01 09:36:09 +0330

    Update urls.py

--- siva/urls.py
+++ siva/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', include('shop.urls')),
     path('cart/', include('cart.urls')),
     path('payment/', include('payment.urls')),
-]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
